5c8607133ec53fc4e4ac1a879063599b886a47d7
mkamins3 <mkamins3@gmail.com>
2021-02-28 18:59:11 -0500

    need to figure out delete button functionality issues

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const addButton = document.querySelector('[data-add-button]');
 const radio = document.getElementsByName('readit');
 const yesRead = document.querySelector('[data-read]').value;
 const notRead = document.querySelector('[data-not-read]').value;
-const selectTD = document.getElementsByTagName('td');
-const deleteBtn = selectTD.getElementsByTagName('button');
+
 addButton.addEventListener('click', addBookToLibrary);
-deleteBtn.addEventListener('click', clearForm)
+
+document.addEventListener('click', function (e) {
+    if (e.target && e.target.hasAttribute('data-delete')) {
+        let delBtnIndex = e.target.getAttribute('data-delete')
+        deleteThisBook(delBtnIndex);
+    }
+})
+
+
 
 function Book(title, author, pages, readIt) {
     //constructor
@@ -15,16 +22,16 @@ function Book(title, author, pages, readIt) {
     this.author = author;
     this.pages = pages;
     this.readIt = readIt;
-    //clearForm()
+    clearForm()
 }
 
 function addBookToLibrary() {
-    
+
     title = document.querySelector('[data-title]').value;
     author = document.querySelector('[data-author]').value;
     pages = document.querySelector('[data-pages]').value;
     readIt = '';
-    
+
     for (var i = 0; i < radio.length; i++) {
         if (radio[i].checked)
             readIt = radio[i].value;
@@ -33,7 +40,7 @@ function addBookToLibrary() {
 
     const newBook = new Book(title, author, pages, readIt);
     myLibrary.push(newBook);
-    
+
     createNewRow(myLibrary);
 }
 
@@ -43,6 +50,7 @@ function createNewRow() {
     let libraryIndex = libraryLength - 1;
 
     let newRow = document.createElement('tr');
+    newRow.setAttribute('data-row', `${libraryIndex}`)
     table.appendChild(newRow);
 
     let newTitle = document.createElement('td');
@@ -63,12 +71,40 @@ function createNewRow() {
 
     let deleteEntry = document.createElement('td');
     let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', 'delete-button',);
+    deleteButton.setAttribute('data-delete', `${libraryIndex}`);
     deleteButton.innerHTML = "delete";
     deleteEntry.appendChild(deleteButton);
     newRow.appendChild(deleteEntry);
-
+    console.log(myLibrary)
     clearForm();
 }
+
+function deleteThisBook(x) {
+
+    let dataRows = document.querySelectorAll('[data-row]')
+
+    myLibrary.splice(x, 1);
+
+
+    table.removeChild(dataRows[x]);
+
+
+
+
+    for (var i = 0; i < (myLibrary.length) - 1; i++) {
+        dataRows.forEach(row => {
+            row.setAttribute('data-row', `${i}`)
+            row.setAttribute('data-delete', `${i}`)
+        })
+    }
+
+};
+
+
+
+
+
 
 function clearForm() {
     document.querySelector('[data-title]').value = '';
